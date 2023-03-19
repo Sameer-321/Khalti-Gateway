3df5c88d0a909bae27e358d7147a599f4d5ace7c
Sameer-321 <snrsameer1@gmail.com>
2023-03-19 14:04:47 +0545

    made react app and khalti installtion and config according to the docs

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Khalti from "./components/Khalti/Khalti";
+
+function App() {
+  return (
+    <>
+      <h2>Khalti integration</h2>
+      <Khalti />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Khalti/Khalti.js
@@ -0,0 +1,8 @@
+import React from "react";
+import KhaltiCheckout from "khalti-checkout-web";
+import config from "./khaltiConfig"
+
+export default function Khalti() {
+  let checkout = new KhaltiCheckout(config);
+  return <button onClick={()=>checkout.show({amount:1000})}>Pay via Khalti</button>;
+}
