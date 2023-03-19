9c7ee8ed98f8eeb1e01598aec366fafc41127f47
Sameer-321 <snrsameer1@gmail.com>
2023-03-19 14:11:00 +0545

    adding style for the button, the UI is given by khalti

--- client/src/components/Khalti/Khalti.js
+++ client/src/components/Khalti/Khalti.js
@@ -1,8 +1,20 @@
 import React from "react";
 import KhaltiCheckout from "khalti-checkout-web";
-import config from "./khaltiConfig"
+import config from "./khaltiConfig";
 
 export default function Khalti() {
   let checkout = new KhaltiCheckout(config);
-  return <button onClick={()=>checkout.show({amount:1000})}>Pay via Khalti</button>;
+  let buttonStyles={
+    backgroundColor:'purple',
+    padding:'10px',
+    color:'white',
+    cursor:'pointer',
+    fontWeight:'bold',
+    border:'1px solid white'
+  }
+  return (
+    <button onClick={() => checkout.show({ amount: 1000 })} style={buttonStyles}>
+      Pay via Khalti
+    </button>
+  );
 }
